e529b5d0032cf2cbb7dfa3f863bc8ea788a9bc6c
Nate-Rod <nate-rod@live.com>
2020-05-10 00:09:02 -0500

    Added (WIP) methods to convert the file into an image.

--- script.js
+++ script.js
@@ -1,6 +1,29 @@
-//Start-up script read test.
-x = 1;
-for(i = 0; i<10; i++){
-  console.log("Count: " + x);
-  x++;
+//Allows for addition of elements to the DOM using a raw string as an input.
+//Very useful for SVGs
+function appendHtml(el, str) {
+  var div = document.createElement('div');
+  div.innerHTML = str;
+  while (div.children.length > 0) {
+    el.appendChild(div.children[0]);
+  }
 }
+
+//Reads text data from an external (local) file
+//and returns the contents as a string.
+function readTextFile(file) {
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", file, true);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4) {
+            if(rawFile.status === 200 || rawFile.status == 0) {
+                var allText = rawFile.responseText;
+                alert(allText);
+            }
+        }
+    }
+    rawFile.send();
+}
+
+//example of above two functions in action:
+sq_nate = readTextFile("squiggle_nate.txt");
+appendHtml(document.getElementById("process"), sq_nate);
