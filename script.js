aaff06ff07621dd55e8bea18a00a6d94d91818bd
Nate-Rod <nate-rod@live.com>
2020-05-10 18:55:59 -0500

    miniupdate: added a unique div container for main page SVG

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 //Allows for addition of SVGs to the DOM.
-function loadSVG(file){
+function loadSVG(id, file){
   xhr = new XMLHttpRequest();
   xhr.open("GET",file,true);
   // Following line is just to be on the safe side;
   xhr.overrideMimeType("image/svg+xml");
   xhr.onload = function(e) {
     // You might also want to check for xhr.readyState/xhr.status here.
-    document.getElementById("process")
+    document.getElementById(id)
       .appendChild(xhr.responseXML.documentElement);
     }
   xhr.send("");
 }
 
-loadSVG("images/blue.svg");
+loadSVG("svgs-main","images/blue.svg");
