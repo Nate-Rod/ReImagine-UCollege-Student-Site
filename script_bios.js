e6fbcf826df5cb742f443f89e9da8673f5525c6a
Nate-Rod <nate-rod@live.com>
2020-05-12 13:23:55 -0500

    Added some skeleton HTML for bio pages

---
+++ script_bios.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.container', { delay: 700, scale: 0.95});
+ScrollReveal().reveal('.load-hidden', { delay: 0, scale: 1});
+
+//Javascript unique to bios should go here and not in the script.js file.
