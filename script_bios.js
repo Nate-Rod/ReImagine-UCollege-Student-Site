a1c9bd951fd893c047f9da07acfcb10eee362b03
Nate-Rod <nate-rod@live.com>
2020-05-13 14:19:22 -0500

    Splash header and some navbar responsiveness

--- script_bios.js
+++ script_bios.js
@@ -2,3 +2,10 @@ ScrollReveal().reveal('.container', { delay: 700, scale: 0.95});
 ScrollReveal().reveal('.load-hidden', { delay: 0, scale: 1});
 
 //Javascript unique to bios should go here and not in the script.js file.
+$(function () {
+  $(document).scroll(function () {
+    var $nav = $(".nav-container-bio");
+    var $splash = $("#bio-splash-nate")
+    $nav.toggleClass('scrolled', $(this).scrollTop() > 1.5*$splash.height());
+  });
+});
