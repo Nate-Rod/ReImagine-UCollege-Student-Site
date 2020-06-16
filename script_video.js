d52b7d66051c51144c8b078bcec1fcc7113e7545
Nate-Rod <nate-rod@live.com>
2020-06-16 12:41:53 -0500

    Centered play button and fluid layout structure

--- script_video.js
+++ script_video.js
@@ -1,15 +1,26 @@
-//create a video player object to use in DOM
-var player = new videojs('vid1', {
-  aspectRatio: '16:9',
-  autoplay: false,
-  controls: true,
-  fill: true,
-  preload: 'auto',
-  responsive: true,
-  sources: [{
-    src: 'videos/test.mp4',
-    type: 'video/mp4'
-  }]
-});
+//video handler functions
+function pauseVideo(event){
+  // `this` is the player in this context
+  this.pause();
+};
 
-$('#v-box').append(player);
+//"constructor" method to build a video given an SRC and an ID
+function buildVideo(vidID, vidSRC){
+  return new videojs(vidID, {
+    aspectRatio: '16:9',
+    autoplay: false,
+    controls: true,
+    fill: true,
+    preload: 'auto',
+    responsive: true,
+    sources: [{
+      src: vidSRC,
+      type: 'video/mp4'
+    }]
+  });
+}
+
+//create a video player object to use in DOM
+$('#v-box').append(
+  buildVideo('vid1', 'videos/test.mp4')
+);
